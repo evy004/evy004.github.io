3996a54482cd08140962980e1e5e64bd999f26a7
evy <evng6281@colorado.edu>
2025-10-30 11:55:03 -0600

    updating html

--- weeklyassignments/wa_11/wa11.js
+++ weeklyassignments/wa_11/wa11.js
@@ -1,18 +1,43 @@
 const dogImg = document.getElementById('dogImg');
-    const btn = document.getElementById('fetchBtn');
+const btn = document.getElementById('fetchBtn');
+const breedSelect = document.getElementById('breedSelect');
 
-    async function fetchDog() {
-      try {
-        const res = await fetch('https://dog.ceo/api/breeds/image/random');
-        const data = await res.json();
-        dogImg.src = data.message;
-        dogImg.alt = "Random dog image";
-      } catch(err) {
-        console.error('Failed to fetch dog image:', err);
-        dogImg.alt = "Could not load dog image :(";
-      }
+async function loadBreeds() {
+  const res = await fetch('https://dog.ceo/api/breeds/list/all');
+  const data = await res.json();
+  const breeds = Object.keys(data.message);
+
+  for (const breed of breeds) {
+    const option = document.createElement('option');
+    option.value = breed;
+    option.textContent = breed[0].toUpperCase() + breed.slice(1);
+    breedSelect.appendChild(option);
+  }
+}
+
+loadBreeds();
+
+async function fetchDog() {
+  try {
+    const breed = breedSelect.value;
+    let url;
+
+    if (breed === 'random') {
+      url = 'https://dog.ceo/api/breeds/image/random';
+    } else {
+      url = `https://dog.ceo/api/breed/${breed}/images/random`;
     }
 
-    btn.addEventListener('click', fetchDog);
+    const res = await fetch(url);
+    const data = await res.json();
 
-    fetchDog();
+    dogImg.src = data.message;
+    dogImg.alt = `${breed} dog image`;
+  } catch (err) {
+    console.error('Error fetching dog image:', err);
+  }
+}
+
+btn.addEventListener('click', fetchDog);
+
+fetchDog();
